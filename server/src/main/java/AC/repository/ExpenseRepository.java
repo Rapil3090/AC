5f01c1900a86cb617e07ce63e708240c4a24ef59
rapil <sjw3090@gmail.com>
2024-06-24 17:50:32 +0900

    조회 기준일 년, 월 기준으로 id조회하여 수입, 지출 총 합 구하는 로직 추가

--- server/src/main/java/AC/repository/ExpenseRepository.java
+++ server/src/main/java/AC/repository/ExpenseRepository.java
@@ -2,9 +2,19 @@ package AC.repository;
 
 import AC.domain.Expense;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
 
 @Repository
 public interface ExpenseRepository extends JpaRepository<Expense, Long> {
 
+
+    @Query("SELECT e FROM Expense e WHERE e.expenseId = :expenseId AND e.year = :year AND e.month = :month")
+    List<Expense> findByExpenseIdAndYearAndMonth(
+            @Param("expenseId") Long expenseId,
+            @Param("year") String year,
+            @Param("month") String month);
 }
--- server/src/main/java/AC/repository/IncomeRepository.java
+++ server/src/main/java/AC/repository/IncomeRepository.java
@@ -2,9 +2,20 @@ package AC.repository;
 
 import AC.domain.Income;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
 
 @Repository
 public interface IncomeRepository extends JpaRepository<Income, Long> {
+
+    @Query("SELECT i FROM Income i WHERE i.incomId = :incomeId AND i.year = :year AND i.month = :month")
+    List<Income> findByIncomeIdAndYearAndMonth(
+            @Param("incomeId") Long incomeId,
+            @Param("year") String year,
+            @Param("month") String month);
+
 
 }
